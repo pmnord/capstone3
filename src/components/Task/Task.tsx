3bdfc525c12cf0798d6cdc111bcb941e99b02c33
Peter <peter.m.nordman@gmail.com>
2021-09-21 20:06:52 -0400

    Refactor Task to tsx

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -7,19 +7,57 @@ import TaskNotes from '../TaskNotes/TaskNotes';
 import DeleteButton from '../DeleteButton/DeleteButton';
 import AddButton from '../AddButton/AddButton';
 
-const Task = (props) => {
+interface Props {
+  addTag: (categoryIndex: number, taskIndex: number, tag: string) => void;
+  addNote: (categoryIndex: number, taskIndex: number, note: string) => void;
+  categoryIndex: number;
+  color: string;
+  deleteNote: (
+    categoryIndex: number,
+    taskIndex: number,
+    noteIndex: number
+  ) => void;
+  deleteTag: (
+    categoryIndex: number,
+    taskIndex: number,
+    tagIndex: number
+  ) => void;
+  deleteTask: (categoryIndex: number, taskIndex: number) => void;
+  hue: string;
+  index: number;
+  notes: string[];
+  tags: string[];
+  title: string;
+  uuid: string;
+}
+
+const Task = ({
+  addNote,
+  addTag,
+  categoryIndex,
+  color,
+  deleteNote,
+  deleteTag,
+  deleteTask,
+  hue,
+  index,
+  notes,
+  tags,
+  title,
+  uuid,
+}: Props) => {
   const [showEditDelete, setShowEditDelete] = useState(false);
 
   function handleDeleteTask() {
-    props.deleteTask(props.categoryIndex, props.index);
+    deleteTask(categoryIndex, index);
   }
 
   return (
-    <Draggable draggableId={props.uuid} index={props.index}>
+    <Draggable draggableId={uuid} index={index}>
       {(provided) => (
         <div>
           <div
-            className={`Task ${props.color && `task--${props.color}`}`}
+            className={`Task ${color && `task--${color}`}`}
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
@@ -29,13 +67,13 @@ const Task = (props) => {
           >
             <div className='Task__body'>
               <div className='Task__header'>
-                <h4>{props.title}</h4>
+                <h4>{title}</h4>
 
                 <div className='Task-header__delete-button-container'>
                   {showEditDelete && (
                     <DeleteButton
-                      hue={props.hue}
-                      id={`cat-${props.categoryIndex}-task-${props.index}-delete`}
+                      hue={hue}
+                      id={`cat-${categoryIndex}-task-${index}-delete`}
                       deleteCallback={handleDeleteTask}
                       thingDeleted='task'
                     />
@@ -43,22 +81,22 @@ const Task = (props) => {
                 </div>
               </div>
 
-              {props.tags.length > 0 && (
+              {tags.length > 0 && (
                 <Tags
-                  tags={props.tags}
-                  color={props.color}
-                  taskIndex={props.index}
-                  deleteTag={props.deleteTag}
-                  categoryIndex={props.categoryIndex}
+                  tags={tags}
+                  color={color}
+                  taskIndex={index}
+                  deleteTag={deleteTag}
+                  categoryIndex={categoryIndex}
                 />
               )}
 
-              {props.notes.length > 0 && (
+              {notes.length > 0 && (
                 <TaskNotes
-                  notes={props.notes}
-                  taskIndex={props.index}
-                  deleteNote={props.deleteNote}
-                  categoryIndex={props.categoryIndex}
+                  notes={notes}
+                  taskIndex={index}
+                  deleteNote={deleteNote}
+                  categoryIndex={categoryIndex}
                 />
               )}
             </div>
@@ -66,18 +104,19 @@ const Task = (props) => {
               <AddButton
                 type='tag'
                 onSubmit={(tag) => {
-                  props.addTag(props.categoryIndex, props.index, tag);
+                  addTag(categoryIndex, index, tag);
                 }}
               />
               <AddButton
                 type='note'
                 onSubmit={(note) => {
-                  props.addNote(props.categoryIndex, props.index, note);
+                  addNote(categoryIndex, index, note);
                 }}
               />
               {/* FUTURE DATE PICKER AND COLOR PICKER */}
               {/* <AddButton type='date' /> */}
               {/* <AddButton type='color' /> */}
+              {/* TODO: hide action buttons when not in use */}
             </div>
           </div>
         </div>
--- src/components/Task/task.test.js
+++ src/components/Task/task.test.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Task from './Task.js';
+import Task from './Task';
 import Category from '../Category/Category.js';
-import Project from '../Project/Project.js';
+import Project from '../Project/Project';
 
 it('Renders the Task component', () => {
-    const div = document.createElement('div');
+  const div = document.createElement('div');
 
-    ReactDOM.render(
-    <Project route={{match: {params: 'foo'}}}>
-        <Category>
-            <Task />
-        </Category>
-    </Project>
-    , div);
+  ReactDOM.render(
+    <Project route={{ match: { params: 'foo' } }}>
+      <Category>
+        <Task />
+      </Category>
+    </Project>,
+    div
+  );
 
-    ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.unmountComponentAtNode(div);
 });
